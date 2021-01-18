8d27912c38718e6494f324ecdbb36968bde71639
Sam Kitonyi <sam.kitonyi@gmail.com>
2021-01-19 01:11:16 +0300

    Introduce authentication (sign up, sign in, sign out)

--- insight/__init__.py
+++ insight/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_required
 from flask_migrate import Migrate
 
 from .app import app as dash_app
@@ -13,7 +14,19 @@ def create_app():
     app.config.from_object(os.environ["APP_SETTINGS"])
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.login"
+    login_manager.init_app(app)
+
     from .models import User
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+    from .auth import auth as auth_blueprint
+
+    app.register_blueprint(auth_blueprint)
     dash_app.init_app(app)
 
     migrate = Migrate(app, db)
