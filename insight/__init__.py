75405d9ce84352ec5a69f34bcb6fc9135c58cdb0
Sam Kitonyi <sam.kitonyi@gmail.com>
2021-01-19 00:55:02 +0300

    Set up persistence with SQLAlchemy and User model

--- insight/__init__.py
+++ insight/__init__.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from .app import app as dash_app
 
 
+db = SQLAlchemy()
 def create_app():
     "Create and configure the app."
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(os.environ["APP_SETTINGS"])
 
+    db.init_app(app)
+    from .models import User
     dash_app.init_app(app)
 
     return app
