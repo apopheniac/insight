214a59553187cd374894983c91ff3c5106454f41
Sam Kitonyi <sam.kitonyi@gmail.com>
2021-01-19 01:20:03 +0300

    Add index page
    Dashboard is no longer the default landing page.

--- insight/__init__.py
+++ insight/__init__.py
@@ -1,4 +1,7 @@
+__version__ = "0.1.0"
+
 import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_required
@@ -8,6 +11,8 @@ from .app import app as dash_app
 
 
 db = SQLAlchemy()
+
+
 def create_app():
     "Create and configure the app."
     app = Flask(__name__, instance_relative_config=True)
@@ -20,13 +25,21 @@ def create_app():
     login_manager.init_app(app)
 
     from .models import User
+
+    from .main import main as main_blueprint
+
+    app.register_blueprint(main_blueprint)
+
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
+
+    # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
 
     app.register_blueprint(auth_blueprint)
+
     dash_app.init_app(app)
 
     migrate = Migrate(app, db)
